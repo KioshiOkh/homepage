931d50bccccb6911a6fa7d1bf34fb5d2448cca5d
Paul | Kioshi <116959356+KioshiOkh@users.noreply.github.com>
2024-04-25 11:33:31 +0200

    Update Wetter.jsx
    deleted key

--- src/web/Wetter.jsx
+++ src/web/Wetter.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import {search, clear, clouds, drizzle, rain, snow, mist, humidity, wind} from '../assets';
 import './wetter.css';
 
-const API_key = "e3801b78e19bd4f12f450c851c54b4f3";
+const API_key = "";
 
 const weatherIcons = {
     '01': clear, // Clear sky
