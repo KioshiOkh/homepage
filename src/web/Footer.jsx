eb27a47fc18da0378a485ef41b62cf68589a06f8
KioshiOk <Emeraldpvp25@gmail.com>
2024-06-13 16:20:43 +0200

    CSR

--- src/web/Footer.jsx
+++ src/web/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom'; // Import Link instead of NavLink for internal links
 import styles from "../style";
 import { footerLinks, socialMedia } from "../const";
 
@@ -16,10 +16,15 @@ const Footer = () => {
               <ul className="list-none mt-3">
                 {link.links.map((items, index) => (
                   <li key={items.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== link.links.length - 1 ? 'mb-2' : 'mb-0'}`}>
-                    {/* Replace anchor tags with Link components */}
-                    <Link to={items.link} target="_blank" rel="noopener noreferrer" className="text-white hover:text-secondary">
-                      {items.name}
-                    </Link>
+                    {items.external ? (
+                      <a href={items.link} target="_blank" rel="noopener noreferrer" className="text-white hover:text-secondary">
+                        {items.name}
+                      </a>
+                    ) : (
+                      <Link to={items.link} className="text-white hover:text-secondary">
+                        {items.name}
+                      </Link>
+                    )}
                   </li>
                 ))}
               </ul>
@@ -34,7 +39,6 @@ const Footer = () => {
         <div className="flex flex-row md:mt-0 mt-6">
           {socialMedia.map((media, index) => (
             <a key={media.id} href={media.link} target="_blank" rel="noopener noreferrer">
-              {/* You can keep social media icons as anchor tags */}
               <img src={media.icon} alt={media.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`} />
             </a>
           ))}
